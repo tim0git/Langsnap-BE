dc83f55238d1ff069eb283c1fd70bef819f4a8ca
tim0git <tim.doolan88@icloud.com>
2020-06-15 09:46:46 +0100

    commit pre merge

--- routes/associations.router.js
+++ routes/associations.router.js
@@ -3,10 +3,15 @@ const {
   associationsWord,
   associationsWordGame,
 } = require("../controller/translate.controller");
+const { auth } = require("../controller/auth.controller");
 const { handle405s } = require("../error/errorHandling");
 
 //get word associations
 associationsRouter.route("/").post(associationsWord).all(handle405s);
-associationsRouter.route("/game").post(associationsWordGame).all(handle405s);
+associationsRouter
+  .route("/game")
+  .post(auth, associationsWordGame)
+  .all(handle405s);
 
 module.exports = { associationsRouter };
+
